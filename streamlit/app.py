b7e45dfdc09bc2aa71f2cc1947191845e0a11c67
kowshik <sbkowshikk@gmail.com>
2024-04-20 21:38:38 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -36,53 +36,54 @@ agent.agent.llm_chain.prompt.template = sys_message
 def chart():
     with st.sidebar:
                 ticker = yf.Ticker(question)
-                htmlcode='''<!-- TradingView Widget BEGIN -->
-            <div class="tradingview-widget-container">
-            <div class="tradingview-widget-container__widget"></div>
-            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js" async>
-            {
-            "symbols": [
-                [
-                {symb}
-                ]
-            ],
-            "chartOnly": false,
-            "width": "100%",
-            "height": "100%",
-            "locale": "en",
-            "colorTheme": "dark",
-            "autosize": true,
-            "showVolume": false,
-            "showMA": false,
-            "hideDateRanges": false,
-            "hideMarketStatus": false,
-            "hideSymbolLogo": false,
-            "scalePosition": "right",
-            "scaleMode": "Normal",
-            "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-            "fontSize": "10",
-            "noTimeScale": false,
-            "valuesTracking": "1",
-            "changeMode": "price-and-percent",
-            "chartType": "area",
-            "maLineColor": "#2962FF",
-            "maLineWidth": 1,
-            "maLength": 9,
-            "backgroundColor": "rgba(14, 17, 23, 1)",
-            "lineWidth": 2,
-            "lineType": 0,
-            "dateRanges": [
-                "1d|1",
-                "1m|30",
-                "3m|60",
-                "12m|1D",
-                "60m|1W",
-                "all|1M"
-            ]
-            }
-            </script>
-            </div>
-            <!-- TradingView Widget END -->'''.format(symb=ticker)
+                htmlcode = '''<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+<div class="tradingview-widget-container__widget"></div>
+<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js" async>
+{
+"symbols": [
+    [
+    "{symb}"  // Insert the ticker symbol here
+    ]
+],
+"chartOnly": false,
+"width": "100%",
+"height": "100%",
+"locale": "en",
+"colorTheme": "dark",
+"autosize": true,
+"showVolume": false,
+"showMA": false,
+"hideDateRanges": false,
+"hideMarketStatus": false,
+"hideSymbolLogo": false,
+"scalePosition": "right",
+"scaleMode": "Normal",
+"fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+"fontSize": "10",
+"noTimeScale": false,
+"valuesTracking": "1",
+"changeMode": "price-and-percent",
+"chartType": "area",
+"maLineColor": "#2962FF",
+"maLineWidth": 1,
+"maLength": 9,
+"backgroundColor": "rgba(14, 17, 23, 1)",
+"lineWidth": 2,
+"lineType": 0,
+"dateRanges": [
+    "1d|1",
+    "1m|30",
+    "3m|60",
+    "12m|1D",
+    "60m|1W",
+    "all|1M"
+]
+}
+</script>
+</div>
+<!-- TradingView Widget END -->'''.format(symb=ticker.info['symbol'])
+
                 components.html(htmlcode,height=550,width=400)
 question=st.chat_input("Ask your stock related Questions")
 if question:
