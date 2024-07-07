3ad1cb0d57cfd2611e6dc20d372395c9f4350085
kowshik <sbkowshikk@gmail.com>
2024-07-07 17:40:59 +0530

    Update stockhelp.py

--- streamlit/stockhelp.py
+++ streamlit/stockhelp.py
@@ -48,4 +48,4 @@ if question:= st.chat_input("Ask your stock related questions"):
     with st.chat_message("assistant",avatar="🦖"):
         st_callback=StreamlitCallbackHandler(st.container())
         response=chain.invoke({"topic": question})
-        st.markdown(create_chat_bubble(response), unsafe_allow_html=True)
+        st.markdown(create_chat_bubble(response), unsafe_allow_html=False)
