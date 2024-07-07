b1cb5ac439d4e829b0a28e15120e4fed0b005959
kowshik <sbkowshikk@gmail.com>
2024-07-07 18:08:17 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,6 +4,7 @@ from langchain.agents import initialize_agent
 from langchain_community.callbacks.streamlit import (
     StreamlitCallbackHandler,
 )
+from pathlib import Path
 from myagent import get_company_symbol,get_stock_price, calculate_rsi, moving_average, predict_stock,candlestick
 import os
 import yfinance as yf
@@ -28,7 +29,7 @@ def create_chat_bubble(text):
 load_dotenv()
 os.environ["HUGGINGFACEHUB_API_KEY"]=st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 os.environ["GOOGLE_API_KEY"]=st.secrets["GOOGLE_API_KEY"]
-
+st.code(Path(".streamlit/config.toml").read_text(), language="toml")
 sth=st.Page("stockhelp.py",title="StockHelp",icon=":material/search:")
 sta=st.Page("stockai.py",title="StockAI",icon=":material/dashboard:")
 pg=st.navigation([sta,sth])
